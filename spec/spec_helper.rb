cd55bc3d28e04681bf0b836507ced1c4a9acdfcf
wehmoreira <william@NESPITHE.(none)>
2014-03-31 11:42:13 -0300

    movendo stub_request do WebMock para o spec_helper e desabilitando a conexao com a internet

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,22 @@ require 'rspec'
 require 'webmock/rspec'
 $LOAD_PATH << './lib'
 require 'filme'
+
+WebMock.disable_net_connect!(allow_localhost: true)
+resource = File.join(File.dirname(__FILE__), 'resources', 'programacao.html')
+
+RSpec.configure do |config|
+  config.before(:each) do
+    stub_request(:get, "www.hagah.com.br/programacao-tv/jsp/default.jspx").
+    with(
+      query: {
+        action: 'busca',
+        gds: '0',
+        operadora: '13',
+        palavras: 'Matrix',
+        skin: '',
+        uf: '26'
+      }
+    ).to_return(body: File.read(resource))
+  end
+end
