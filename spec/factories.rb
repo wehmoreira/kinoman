2ba794a26dba9dd1a99d84dedfde221d0a5a89b5
wehmoreira <wehmoreira@gmail.com>
2014-04-23 16:57:32 -0300

    spec da classe Filme + arquivos de retorno para os testes + factories

---
+++ spec/factories.rb
@@ -0,0 +1,31 @@
+FactoryGirl.define do
+  [
+    ["Sáb - 29/03", "17:30", "Warner"],    ["Sáb - 29/03", "17:30", "Warner HD"],
+    ["Dom - 30/03", "00:35", "TNT"],       ["Dom - 30/03", "13:00", "TNT"],
+    ["Dom - 30/03", "15:20", "Warner"],    ["Dom - 30/03", "15:20", "Warner HD"],
+    ["Sex - 28/03", "09:14", "Space"],     ["Sex - 28/03", "09:14", "Space HD"],
+    ["Dom - 06/04", "00:25", "Warner"],    ["Dom - 06/04", "00:25", "Warner HD"],
+    ["Dom - 06/04", "22:00", "Warner HD"], ["Dom - 06/04", "22:00", "Warner"]
+  ].each_with_index do |value, key|
+    factory "prog_#{key}".to_sym, class: Programacao do
+      initialize_with{ new(*value) }
+    end
+  end
+
+  factory :filme do
+    initialize_with{ new('Filme', 'Matrix') }
+    programacao do
+      [ build(:prog_0), build(:prog_1), build(:prog_2),
+        build(:prog_3), build(:prog_4), build(:prog_5) ]
+    end
+  end
+
+  factory :sequel, class: Filme do
+    initialize_with{ new('Filme', 'Matrix Regurgitations') }
+    programacao do
+      [ build(:prog_6), build(:prog_7), build(:prog_8),
+        build(:prog_9), build(:prog_10), build(:prog_11) ]
+     end
+  end
+
+end
--- spec/filme_spec.rb
+++ spec/filme_spec.rb
@@ -1,14 +1,52 @@
 require 'spec_helper'
 filme = 'Matrix'
 
+# cria um array de objetos recebidos e esperados, do tipo
+# [ "Matrix", "Matrix",
+#   "Filme", "Filme",
+#   "Dom - 30/03", "Dom - 30/03", "00:35", "00:35", "TNT", "TNT",
+#   "Dom - 30/03", "Dom - 30/03", "13:00", "13:00", "TNT", "TNT",
+#   "Dom - 30/03", "Dom - 30/03", "15:20", "15:20", "Warner", "Warner" ]
+def descanter(array, ary_expected)
+  array.zip(ary_expected).map do |object, obj_expected|
+    object.instance_variables.map do |method|
+      m = method.to_s.gsub('@', '').to_sym
+      if m == :programacao
+        descanter(object.programacao, obj_expected.programacao)
+      else
+        [ object.send(m), obj_expected.send(m) ]
+      end
+    end
+  end.flatten
+end
 
 describe Filme do
 
   subject { Filme.new('Filme', filme) }
-  its(:programacao) { should be_kind_of Array }
+  its(:programacao) { should be_an Array }
 
   describe '.find' do
     subject { Filme.find(filme) }
+
+    it { should be_an Array }
+
     its(:size) { should eq(2) }
+
+    it 'deve ser um array de filmes contendo um array de programacoes' do
+      subject.each do |film|
+        film.should be_a Filme
+        film.programacao.each do |prog|
+          prog.should be_a Programacao
+        end
+      end
+    end
+
+    it 'deve conter todas as informacoes de programacao adequadamente' do
+      array = descanter(subject, [build(:filme), build(:sequel)])
+      array.each_slice(2) do |result, expected|
+        result.should eq(expected)
+      end
+    end
+
   end
 end
