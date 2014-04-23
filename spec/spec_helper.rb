eb2afc722b60ad8578180f387739b8e9cfbc15fb
wehmoreira <wehmoreira@gmail.com>
2014-04-23 16:55:22 -0300

    stubs das requisicoes feitas

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,23 @@ require 'rspec'
 require 'webmock/rspec'
 $LOAD_PATH << './lib'
 require 'filme'
+require 'watchlist'
+require 'factory_girl'
+
+resources = File.join(File.dirname(__FILE__), 'resources')
+
+FactoryGirl.find_definitions
 
 WebMock.disable_net_connect!(allow_localhost: true)
-resource = File.join(File.dirname(__FILE__), 'resources', 'programacao.html')
+hagah = resources + '/programacao.html'
+imdb  = resources + '/watchlist.html'
 
 RSpec.configure do |config|
+  # including factory_girl
+  config.include FactoryGirl::Syntax::Methods
+  # stubs for http requests
   config.before(:each) do
+    # stub for Filme class
     stub_request(:get, "www.hagah.com.br/programacao-tv/jsp/default.jspx").
     with(
       query: {
@@ -18,6 +29,9 @@ RSpec.configure do |config|
         skin: '',
         uf: '26'
       }
-    ).to_return(body: File.read(resource))
+    ).to_return(body: File.read(hagah))
+    # stub for Watchlist class
+    stub_request(:get, "http://www.imdb.com/user/ur52111019/watchlist")
+    .to_return(body: File.read(imdb), headers: { 'Content-Type' => 'text/html' })
   end
 end
